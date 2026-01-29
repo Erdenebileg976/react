7556c5b8265094b29860e01e9be21dd3da97ec66
BACK-10 <back-10@Erxess-Mac-mini-4.local>
2026-01-29 19:53:30 +0800

    3

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [click, setClick] = useState("👁 Reveal Secret");
+  //
+  const [isToggled, setIsToggled] = useState(false);
 
+  // 2. Create an event handler to toggle the state
+  const handleClick = () => {
+    // Recommended way: use a function param to get the previous value
+    setIsToggled((prevIsToggled) => !prevIsToggled);
+  };
+  // console.log(click, "asdsa");
+
+  // const handleClick = () => {
+  //   console.log("jdhjdhkjzh");
+  // };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>🟢 Ex 3: Toggle Visibility</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleClick}>
+          {isToggled ? "🙈 Hide Secret" : "👁 Reveal Secretecret"}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+        {/* Conditionally render content based on the boolean state */}
+        {isToggled && <p>✨ React is awesome! ✨</p>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
