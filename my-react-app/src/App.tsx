d4bc225d6690902fc0a8edf3927e7afa92710b13
BACK-10 <back-10@Erxess-Mac-mini-4.local>
2026-01-29 20:59:38 +0800

    4

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [click, setClick] = useState("");
+  const [age, setAge] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>🟢 Ex 4: Props & Cards</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <article>
+          <input type="text" onChange={(e) => setClick(e.target.value)} />
+          <button id="createBtn">name {click}</button>
+        </article>
+        <input type="number" onChange={(e) => setAge(e.target.value)} />
+        <button id="createBtn">age {age}</button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {click}
+          {age}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
